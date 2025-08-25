f052f0d80f61f9250e85a2b085ec299cf026e7b6
ThenulaD <thenulive@outlook.com>
2025-08-25 11:45:19 +0530

    Adding Springboot Plain Files

---
+++ src/test/java/com/example/lifesureinsuarncemanagementsystem/LifeSureInsuarnceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifesureinsuarncemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeSureInsuarnceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
