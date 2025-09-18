0e65b7a4edd0e14fe09a44b725a0d56c48109f66
Thenula Dewanmith <it24102533@my.sliit.lk>
2025-09-18 18:26:01 +0530

    Update LifeSureInsuarnceManagementSystemApplication.java

--- src/main/java/com/example/lifesureinsuarncemanagementsystem/LifeSureInsuarnceManagementSystemApplication.java
+++ src/main/java/com/example/lifesureinsuarncemanagementsystem/LifeSureInsuarnceManagementSystemApplication.java
@@ -9,5 +9,4 @@ public class LifeSureInsuarnceManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LifeSureInsuarnceManagementSystemApplication.class, args);
 	}
-
 }
